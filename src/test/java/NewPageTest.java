79b6b8767a66d8f538a5a6044180820324cb3621
Hannatty Mendonça Oliveira Barros <hanatxy@gmail.com>
2022-12-01 00:45:53 -0300

    Adjusts and new test

--- src/test/java/NewPageTest.java
+++ src/test/java/NewPageTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import java.util.List;
 
 @TestInstance(Lifecycle.PER_CLASS)
@@ -34,17 +35,16 @@ public class NewPageTest {
     webdriver.close();
   }
 
-
+  //Site do Condomínio Vivenda Beira Rio
   @Test
   public void openNewPage(){
     webdriver.get("https://www.vivendabeirario.com/");
-    Assertions.assertEquals("https://www.vivendabeirario.com/",
-        webdriver.getCurrentUrl());
+    Assertions.assertEquals("https://www.vivendabeirario.com/", webdriver.getCurrentUrl());
   }
 
   @Test
   public void openNemPageFalse() {
-    webdriver.get("http://altinho.pe.gov.br/v1/");
+    webdriver.get("https://www.vivendabeirario.com/");
     boolean tagTitle = webdriver.getTitle().equalsIgnoreCase("Condomínio Vivenda Beira Rio");
     Assertions.assertFalse(tagTitle);
   }
@@ -58,9 +58,9 @@ public class NewPageTest {
   }
 
   @Test
-  public void FindCoronaLink(){
+  public void FindLink(){
     webdriver.get("https://www.vivendabeirario.com/");
-    WebElement coronaLink = webdriver.findElement(
+    WebElement Link = webdriver.findElement(
             By.xpath("/html/body/div/div/div[3]/div/main/div/div/div/div[2]/div/div/div/div[2]/div[2]/div/div[10]/a/wix-image/img"));
   }
 
@@ -78,21 +78,13 @@ public class NewPageTest {
     System.out.println("Number of elements:" +elements.size());
   }
 
-  //Prefeitura de Altinho
+  //Site da Prefeitura de Altinho
 
   @Test
   public void openPrefeituraPage(){
     webdriver.get("http://altinho.pe.gov.br/v1");
     Assertions.assertEquals("https://www.vivendabeirario.com/",
             webdriver.getCurrentUrl());
-  }
-
-  @Test
-  public void searchInPrefeituraPage (){
-    webdriver.get("http://altinho.pe.gov.br/v1/");
-    WebElement search = webdriver.findElement(By.id("s"));
-    search.sendKeys("vacina");
-    search.submit();
   }
 
   @Test
@@ -105,9 +97,27 @@ public class NewPageTest {
   }
 
   @Test
+  public void searchInPrefeituraPage (){
+    webdriver.get("http://altinho.pe.gov.br/v1/");
+    WebElement search = webdriver.findElement(By.id("s"));
+    search.sendKeys("vacina");
+    search.submit();
+  }
+
+  @Test
   public void openPrefeituraPageFalse() {
     webdriver.get("http://altinho.pe.gov.br/v1/");
     boolean verifyTagTitle = webdriver.getTitle().equalsIgnoreCase("Prefeitura de Altinho");
     Assertions.assertFalse(verifyTagTitle);
   }
+
+  //Site Sebrae
+  @Test
+  public void selectIndexElement() {
+    webdriver.get("https://sebrae.com.br/sites/PortalSebrae");
+    Select menuItem = new Select(webdriver.findElement(By.id("selectModalLocation")));
+    menuItem.selectByIndex(1);
+  }
 }
+
+
